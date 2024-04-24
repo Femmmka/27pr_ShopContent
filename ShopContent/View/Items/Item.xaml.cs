c87ab2e60e86a1bfe59f2c917bca0be6dd1e0c5a
Ефим <fimakrut@mail.ru>
2024-04-24 12:44:18 +0500

    Разработанные классы общего пользования. Ярыгин

--- ShopContent/View/Items/Item.xaml.cs
+++ ShopContent/View/Items/Item.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ShopContent.Items
+namespace ShopContent.View.Items
 {
     /// <summary>
     /// Логика взаимодействия для Item.xaml
