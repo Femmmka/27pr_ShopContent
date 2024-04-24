fd785f1179c33372749a9c924aca36ac0ec18d6b
Ефим <fimakrut@mail.ru>
2024-04-24 12:59:30 +0500

    Разработанные модели данных. Ярыгин

---
+++ ShopContent/Modell/Categorys.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ShopContent.Modell
+{
+    public class Categorys : INotifyPropertyChanged
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnProperyChanged("Id");
+            }
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnProperyChanged("name");
+            }
+        }
+        public event PropertyChangedEventHandler ProperyChanged;
+        public void OnProperyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangingEventArgs(prop));
+        }
+    }
+}
---
+++ ShopContent/Modell/Items.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System;
+
+namespace ShopContent.Modell
+{
+    public class Items : INotifyPropertyChanged
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set{
+                id = value;
+                OnProperyChanged("Id");
+            }
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnProperyChanged("Name");
+            }
+        }
+        private double price;
+        public double Price
+        {
+            get { return Price; }
+            set
+            {
+                price = value;
+                OnProperyChanged("Price");
+            }
+        }
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                description = value;
+                OnProperyChanged("Description");
+            }
+        }
+        private Categorys category;
+        public Categorys Category
+        {
+            get { return category; }
+            set
+            {
+                category = value;
+                OnProperyChanged("Category");
+            }
+        }
+        public event PropertyChangedEventHandler ProperyChanged;
+        public void OnProperyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangingEventArgs(prop));
+        }
+
+    }
+}
